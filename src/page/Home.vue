361137546c736afd1d3211e2d7b98400a6babf62
Nyan Myo Htet <40001598+nyanmyohtet@users.noreply.github.com>
2023-11-14 00:27:35 +0630

    Develop (#1)
    * disable notification
    
    * implement ui

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -1,47 +1,59 @@
 <script setup lang="ts">
-import { NButton, NCard, NDropdown } from 'naive-ui'
-import { useMessage } from 'naive-ui'
+import { ref } from 'vue'
+// import { useMessage } from 'naive-ui';
+import { MdAdd, MdRemove } from '@vicons/ionicons4'
 
-const message = useMessage()
-const options = [
-  {
-    label: 'Marina Bay Sands',
-    key: 'marina bay sands',
-    disabled: true
-  },
-  {
-    label: "Brown's Hotel, London",
-    key: "brown's hotel, london"
-  },
-  {
-    label: 'Atlantis Bahamas, Nassau',
-    key: 'atlantis nahamas, nassau'
-  },
-  {
-    label: 'The Beverly Hills Hotel, Los Angeles',
-    key: 'the beverly hills hotel, los angeles'
+// const message = useMessage()
+const textInput = ref('')
+const taskList = ref([])
+
+const onSubmit = () => {
+  if (textInput.value.length > 0) {
+    taskList.value.push(textInput.value)
+    textInput.value = ''
   }
-]
-const handleSelect = (key: string | number) => {
-  message.info(String(key))
+}
+
+const removeTask = (index: any) => {
+  taskList.value.splice(index, 1)
 }
 </script>
 
 <template>
   <div>
-    <n-card title="Card">
-      Card Content
-      <n-button>naive-ui</n-button>
-    </n-card>
-
-    <n-dropdown trigger="hover" :options="options" @select="handleSelect">
-      <n-button>Go For a Trip</n-button>
-    </n-dropdown>
+    <n-layout style="height: 340px; position: relative">
+      <n-layout-content position="absolute">
+        <n-card size="small" :bordered="false">
+          <n-list hoverable>
+            <n-list-item v-for="(task, index) in taskList">
+              <n-thing :title="task"></n-thing>
+              <template #suffix>
+                <n-button size="large" @click="removeTask(index)">
+                  <template #icon><n-icon><md-remove /></n-icon></template>
+                </n-button>
+              </template>
+            </n-list-item>
+          </n-list>
+        </n-card>
+      </n-layout-content>
+      <n-layout-footer bordered position="absolute" style="height: 65px; padding: 0 10px">
+        <n-form :style="{ maxWidth: '341px' }">
+          <n-form-item>
+            <n-input v-model:value="textInput" type="text" size="large" placeholder="New Task" />
+            <n-button size="large" style="margin-left: 12px" @click="onSubmit">
+              <template #icon>
+                <n-icon><md-add /></n-icon>
+              </template>
+            </n-button>
+          </n-form-item>
+        </n-form>
+      </n-layout-footer>
+    </n-layout>
   </div>
 </template>
 
 <style scoped>
-.n-card {
-  max-width: 300px;
+.n-form-item {
+  --n-label-height: 14px !important;
 }
 </style>
